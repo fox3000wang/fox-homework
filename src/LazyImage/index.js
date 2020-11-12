9ac6ebbcf1255509d768d224fc295a93f0e3272f
fox.wang <fox3000wang@hotmail.com>
2020-11-12 16:26:48 +0800

    初步配置好webpack的打包和开发环境

---
+++ src/LazyImage/index.js
@@ -0,0 +1,68 @@
+class LazyImage {
+  constructor(options) {
+    // init params
+    options = options || {};
+    let defaults = {
+      context: document,
+      attr: 'lazy-image',
+      threshold: 1,
+      speed: 300,
+      callback: Function.prototype,
+    };
+    init(Object.assign(defaults, options));
+  }
+
+  init(config) {
+    // 把信息挂在到实例上：在其它方法中，基于实例即可获取这些信息
+    this.config = config;
+    this.imageBoxList = [];
+
+    // 创建监听器
+    const oboptions = {
+      threshold: [config.threshold],
+    };
+    this.ob = new IntersectionObserver(changes => {
+      changes.forEach(item => {
+        let { isIntersecting, target } = item;
+        if (isIntersecting) {
+          this.singleHandle(target);
+          this.ob.unobserve(target);
+        }
+      });
+    }, oboptions);
+    this.observeAll();
+  }
+
+  // 单张图片的延迟加载
+  singleHandle(imgBox) {
+    let config = this.config,
+      imgObj = imgBox.querySelector('img'),
+      trueImage = imgObj.getAttribute(config.attr);
+    imgObj.src = trueImage;
+    imgObj.removeAttribute(config.attr);
+    imgObj.onload = () => {
+      imgObj.style.transition = `opacity ${config.speed}ms`;
+      imgObj.style.opacity = 1;
+      // 回调函数->插件的生命周期函数「回调函数 & 发布订阅」
+      config.callback.call(this, imgObj);
+    };
+  }
+
+  // 监听需要的DOM元素
+  observeAll(refresh) {
+    let config = this.config,
+      allImages = config.context.querySelectorAll(`img[${config.attr}]`);
+    [].forEach.call(allImages, item => {
+      let imageBox = item.parentNode;
+      if (refresh && this.imageBoxList.includes(imageBox)) return;
+      this.imageBoxList.push(imageBox);
+      this.ob.observe(imageBox);
+    });
+  }
+
+  // 刷新：获取新增的需要延迟加载的图片，做延迟加载
+  refresh() {
+    this.observeAll(true);
+  }
+}
+export default LazyImage;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: 'development',
+  entry: './src/LazyImage/index.js',
+  //   devtool: 'cheap-module-eval-source-map',
+
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devServer: {
+    port: 8888,
+    open: true,
+    compress: true,
+    contentBase: path.resolve(__dirname, 'static'),
+    hot: true,
+  },
+
+  plugins: [
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+      inject: true,
+      minify: {
+        minifyJS: true,
+      },
+    }),
+  ],
 };
