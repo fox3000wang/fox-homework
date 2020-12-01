37361946ef335e195eb19ee30a40ac14d0932b15
fox.wang <fox3000wang@hotmail.com>
2020-12-01 13:33:46 +0800

    绘制树流程完成

--- src/page06.js
+++ src/page06.js
@@ -1,10 +1,54 @@
+let root = document.getElementById('root');
+
+const data = {
+  a: 'a',
+  b: {
+    b1: 'b1',
+    b2: 'b2',
+    b3: 'b3',
+    c: {
+      c: 'ccc',
+      x: {
+        y: 'y',
+      },
+    },
+  },
+  d: 'd',
+};
+
+function renderTree(data, root) {
+  if (!root) {
+    root = document.createElement('div');
+  }
+
+  Object.keys(data).forEach(e => {
+    let value = data[e];
+    if (value.toString() === `[object Object]`) {
+      let father = document.createElement('div');
+      father.className = 'father';
+      root.appendChild(father);
+
+      let node = document.createElement('div');
+      node.innerHTML = '-';
+      node.className = 'node';
+      father.appendChild(node);
+
+      renderTree(value, father);
+    } else {
+      let div = document.createElement('div');
+      div.className = 'child';
+      div.innerHTML = value;
+      root.appendChild(div);
+    }
+  });
+  return root;
+}
+
+let tree = renderTree(data);
+root.appendChild(tree);
+
 let node1 = document.getElementById('node1');
-
 let father1 = document.getElementById('father1');
-
-node1.addEventListener('click', clickHandler);
-
-//father1.addEventListener('click', statusCheck);
 
 function clickHandler(e) {
   let target = e.target;
@@ -27,8 +71,3 @@ function clickHandler(e) {
     }
   }
 }
-
-// function statusCheck(e) {
-//   let target = e.target;
-//   console.log(e);
-// }
