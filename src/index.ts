2635ae28abf2242ad332f5c1865e803312f439d4
fox.wang <fox3000wang@hotmail.com>
2020-12-15 11:17:43 +0800

    page2 完成

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // <- vscode 这里提示警告, 不知道为什么
 
 import page01 from './page01';
+import page02 from './page02';
 import page03 from './page03';
 import page05 from './page05';
 import page06 from './page06';
@@ -9,6 +10,7 @@ import page08 from './page08';
 require('./css/reset.css');
 require('./css/main.css');
 require('./css/page01.css');
+require('./css/page02.css');
 require('./css/page06.css');
 require('./css/page05.css');
 require('./css/page08.css');
@@ -19,7 +21,7 @@ require('./css/page08.css');
 const root = document.getElementById('root');
 const btn = document.getElementById('btn');
 
-const funs:Array<Function> = [null,page01,null,page03,null,page05,page06,null,page08];
+const funs:Array<Function> = [null,page01,page02,page03,null,page05,page06,null,page08];
 
 for(let i = 1; i < 11; i++){
   const div = document.createElement('div');
