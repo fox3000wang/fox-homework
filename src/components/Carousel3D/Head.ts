b910a094ced6d16fd14af9fe77158f72a33dde9a
fox.wang <fox3000wang@hotmail.com>
2020-12-18 12:04:17 +0800

    重构初步完成

--- src/components/Carousel3D/Head.ts
+++ src/components/Carousel3D/Head.ts
@@ -1,4 +1,17 @@
 
 export class Head{
 
+  private root:HTMLElement;
+  private lis:HTMLCollection;
+
+  constructor(element:HTMLElement){
+    
+    this.root = element;
+    this.lis = element.getElementsByTagName('li');
+
+
+
+  }
+
+
 }
--- src/components/Carousel3D/index.ts
+++ src/components/Carousel3D/index.ts
@@ -1,3 +1,170 @@
+
+
+
+class Head extends Object{
+
+  public static readonly ON_CHANGE:string = 'ON_CHANGE';
+  private lis:HTMLCollection;
+  private current:number = 0;
+
+  public getCurrent():number{
+    return this.current;
+  }
+  public setCurrent(current:number):void{
+    if(this.current !== current){
+      this.current = current;
+      this.tab();
+    }
+  }
+  
+  constructor(element:HTMLElement){
+    super();
+    this.lis = element.getElementsByTagName('li');
+    this.init();
+  }
+
+  // 初始化,一般只执行一次
+  init = () => {
+    for (let i:number = 0; i < this.lis.length; i++) {
+      const li:any = this.lis[i];
+      li.index = i;
+      li.addEventListener('mouseover', (e:MouseEvent) => {
+        startMove(e.currentTarget, { opacity: 100 });
+      });
+      li.addEventListener('mouseout', (e:any) => {
+        if (e.currentTarget.className !== 'hove'){
+          startMove(e.currentTarget, { opacity: 40 });
+        }
+      });
+      li.addEventListener('click', (e:any) => {
+        this.current = e.currentTarget.index;
+        this.tab();
+        this.onChange();
+      });
+    }
+    this.tab();
+  }
+
+  // 小图标点击
+  tab = () => {
+    for (let i:number = 0; i < this.lis.length; i++) {
+      const li:any = this.lis[i];
+      li.className = '';
+      startMove(li, { opacity: 40 });
+    }
+    this.lis[this.current].className = 'hove';
+    startMove(this.lis[this.current], { opacity: 100 });
+  }
+
+  onChange = () => {
+    const event:CustomEvent = new CustomEvent(Head.ON_CHANGE);
+    dispatchEvent(event);
+  }
+
+}
+
+class Body extends Object{
+  
+  private readonly position:Array<object> = [
+    { width: 344, height: 440, top: 0, left: 352, zIndex: 10 },
+    { width: 260, height: 332, top: 56, left: 148, zIndex: 8 },
+    { width: 204, height: 260, top: 92, left: 0, zIndex: 6 },
+    { width: 140, height: 180, top: 132, left: 148, zIndex: 4 },
+    { width: 110, height: 140, top: 172, left: 232, zIndex: 2 },
+    { width: 110, height: 140, top: 172, left: 708, zIndex: 2 },
+    { width: 140, height: 180, top: 132, left: 770, zIndex: 4 },
+    { width: 204, height: 260, top: 92, left: 844, zIndex: 6 },
+    { width: 260, height: 332, top: 56, left: 640, zIndex: 8 },
+  ];
+  private target:Array<object> = [];
+  private lis:HTMLCollection;
+  private current:number = 0;
+
+  constructor(element:HTMLElement){
+    super();
+    this.lis = element.getElementsByTagName('li');
+    this.init();
+  }
+
+  init = () => {
+    for (let i = 0; i < this.lis.length; i++) {
+      const li:any = this.lis[i];
+      const pos:any = this.position[i];
+      this.target[i] = pos;
+      li.index = i;
+      li.style.width = `${pos.width}px`;
+      li.style.height = `${pos.height}px`;
+      li.style.top = `${pos.top}px`;
+      li.style.left = `${pos.left}px`;
+      li.style.zIndex = pos.zIndex;
+      
+      myAddEvent(li, 'mouseover', (e:MouseEvent) => {
+        let element:HTMLElement = e.currentTarget as HTMLElement;
+        if(element.localName === 'li'){
+          element = element.getElementsByTagName('div')[0];
+          startMove(element, { opacity: 0 });
+        }
+      });
+
+      myAddEvent(li, 'mouseout', (e:MouseEvent) => {
+        const li:any = e.currentTarget;
+        let div:HTMLElement;
+        if(li.localName === 'li'){
+          div = li.getElementsByTagName('div')[0];
+        }
+        if(this.current === li.index){
+          startMove(div, { opacity: 0 });
+        } else {
+          startMove(div, { opacity: 75 });
+        }
+      });
+
+      myAddEvent(li, 'click', (e:MouseEvent) => {
+        const li:any = e.currentTarget;
+        this.current = li.index;
+  
+        this.reset();
+        this.sort();
+        this.move();
+      });
+      
+      this.one();
+    }
+  }
+
+  reset = () => {
+    this.position.forEach((e,i)=> this.target[i] = e);
+  }
+
+  sort = () => {
+    for (let i:number = 0; i < this.current; i++) {
+      this.target.unshift(this.target.pop());
+    }
+  }
+
+  move = () => {
+    for (let i:number = 0; i < this.lis.length; i++) {
+      const li:any = this.lis[i];
+      const div = li.getElementsByTagName('div')[0];
+      startMove(div, { opacity: 75 });
+      startMove(li, this.target[i], this.one);
+    }
+    this.lis[this.current].className = 'hove';
+  }
+
+  // 动画播放完毕以后, 把当前的一张遮罩去掉
+  one = () => {
+    for (let i:number = 0; i < this.lis.length; i++) {
+      const li:any = this.lis[i];
+      if (li.index === this.current) {
+        let oDiv = li.getElementsByTagName('div')[0];
+        startMove(oDiv, { opacity: 0 });
+      }
+    }
+  }
+
+}
+
 /**
  * 3D轮播图组件
  * 
@@ -13,216 +180,90 @@
  *    5.设计得比较好的一点就是通过数值来驱动画面的变化。
  */
 export class Carousel3D{
-
-  private position:object = [
-    { width: 344, height: 440, top: 0, left: 352, zIndex: 10 },
-    { width: 260, height: 332, top: 56, left: 148, zIndex: 8 },
-    { width: 204, height: 260, top: 92, left: 0, zIndex: 6 },
-    { width: 140, height: 180, top: 132, left: 148, zIndex: 4 },
-    { width: 110, height: 140, top: 172, left: 232, zIndex: 2 },
-    { width: 110, height: 140, top: 172, left: 708, zIndex: 2 },
-    { width: 140, height: 180, top: 132, left: 770, zIndex: 4 },
-    { width: 204, height: 260, top: 92, left: 844, zIndex: 6 },
-    { width: 260, height: 332, top: 56, left: 640, zIndex: 8 },
-  ];
+  
+  private head:Head;
+  private body:Body;
 
   constructor(){
 
     let oTop = document.getElementById('top');
+    this.head = new Head(oTop);
+
     let oBut = document.getElementById('list');
+    this.body = new Body(oBut);
+
+    //this.head.
     
-    let oTli = oTop.getElementsByTagName('li');
-    let aLi = oBut.getElementsByTagName('li');
-    
-    let i = 0;
-    let iNow = 0;
-
-    
-    let aSort:any = [];
-
-    // 初始化小图标部分
-    for (i = 0; i < oTli.length; i++) {
-      oTli[i].index = i;
-
-      this.myAddEvent(oTli[i], 'mouseover', (e:MouseEvent) => {
-        this.startMove(e.currentTarget, { opacity: 100 });
-      });
-      this.myAddEvent(oTli[i], 'mouseout', (e:MouseEvent) => {
-        if (e.currentTarget.className !== 'hove'){
-          this.startMove(e.currentTarget, { opacity: 40 });
-        } 
-      });
-      this.myAddEvent(oTli[i], 'click', (e:MouseEvent) => {
-        iNow = e.currentTarget.index;
-        tab();
-      });
-    }
-
-    // 初始化大图
-    for (i = 0; i < aLi.length; i++) {
-      aLi[i].index = i;
-      aLi[i].style.width = this.position[i].width + 'px';
-      aLi[i].style.height = this.position[i].height + 'px';
-      aLi[i].style.top = this.position[i].top + 'px';
-      aLi[i].style.left = this.position[i].left + 'px';
-      aLi[i].style.zIndex = this.position[i].zIndex;
-      aSort[i] = this.position[i];
-
-      this.myAddEvent(aLi[i], 'mouseover', (e:MouseEvent) => {
-        let element:HTMLElement = e.currentTarget as HTMLElement;
-        if(element.localName === 'li'){
-          element = element.getElementsByTagName('div')[0];
-          this.startMove(element, { opacity: 0 });
-        }
-      });
-
-      this.myAddEvent(aLi[i], 'mouseout', (e:MouseEvent) => {
-        const li:HTMLElement = e.currentTarget as HTMLElement;
-        let div:HTMLElement;
-        if(li.localName === 'li'){
-          div = li.getElementsByTagName('div')[0];
-        }
-        // if (li.style.width === '344px') {
-        if(iNow === li.index){
-          this.startMove(div, { opacity: 0 });
-        } else {
-          this.startMove(div, { opacity: 75 });
-        }
-      });
-
-      this.myAddEvent(aLi[i], 'click', (e:MouseEvent) => {
-        let iSort = e.currentTarget.index;
-        iNow = e.currentTarget.index;
-        sort();
-        for (i = 0; i < iSort; i++) {
-          aSort.unshift(aSort.pop());
-        }
-        sMove();
-      });
-    }
-
-    // 小图标点击
-    const tab = () => {
-      for (i = 0; i < oTli.length; i++) {
-        (oTli[i].className = ''), this.startMove(oTli[i], { opacity: 40 });
-      }
-      oTli[iNow].className = 'hove';
-      this.startMove(oTli[iNow], { opacity: 100 });
-      let iSort = iNow;
-      sort();
-      for (i = 0; i < iSort; i++) {
-        aSort.unshift(aSort.pop());
-      }
-      sMove();
-    }
-
-    const sort = () => {
-      for (i = 0; i < aLi.length; i++) {
-        aSort[i] = this.position[i];
-      }
-    }
-
-    const sMove = () => {
-      for (i = 0; i < aLi.length; i++) {
-        let oDiv = aLi[i].getElementsByTagName('div')[0];
-        this.startMove(oDiv, { opacity: 75 });
-        
-        this.startMove(aLi[i], aSort[i], one);
-        aLi[i].className = '';
-      }
-      aLi[iNow].className = 'hove';
-    }
-
-    const one = () => {  
-      for (i = 0; i < aLi.length; i++) {
-        // 这里也会由于精度达不到,所以也要取舍
-        //if (aLi[i].style.width === '344px') {
-        if (parseFloat(aLi[i].style.width) >= 340) {
-          let oDiv = aLi[i].getElementsByTagName('div')[0];
-          this.startMove(oDiv, { opacity: 0 });
-        }
-      }
-    }
-
-    one();
-
     // 自动翻页
-    let timer:any = null;
-    let oSmall = this.getClass(oTop, 'small')[0];
-    const autoLoop = () => {
-      oSmall.onmouseover = oBut.onmouseover = e => {
-        clearInterval(timer);
-      };
-      oSmall.onmouseout = oBut.onmouseout = e => {
-        clearInterval(timer);
-        timer = setInterval(setInter, 5000);
-      };
-      timer = setInterval(setInter, 5000);
+    // let timer:any = null;
+    // //let oSmall = this.getClass(oTop, 'small')[0];
+    // let oSmall = oTop.getElementsByClassName('small')[0];
+    // const autoLoop = () => {
+    //   oSmall.onmouseover = oBut.onmouseover = e => {
+    //     clearInterval(timer);
+    //   };
+    //   oSmall.onmouseout = oBut.onmouseout = e => {
+    //     clearInterval(timer);
+    //     timer = setInterval(setInter, 5000);
+    //   };
+    //   timer = setInterval(setInter, 5000);
   
-      function setInter() {
-        iNow++;
-        if (iNow > aLi.length - 1) {
-          iNow = 0;
-        }
-        tab();
-      }
-    }
-    autoLoop();
+    //   function setInter() {
+    //     iNow++;
+    //     if (iNow > aLi.length - 1) {
+    //       iNow = 0;
+    //     }
+    //     tab();
+    //   }
+    // }
+    // autoLoop();
   }
+  
+}
 
-  getClass = (oParent, sClass) => {
-    let aElem = document.getElementsByTagName('*');
-    let aClass = [];
-    let i = 0;
-    for (i = 0; i < aElem.length; i++) {
-      if (aElem[i].className === sClass) {
-        aClass.push(aElem[i]);
-      }
-    }
-    return aClass;
+
+function myAddEvent(obj, sEvent, fn){
+  if (obj.attachEvent) {
+    obj.attachEvent('on' + sEvent, function () {
+      fn.call(obj);
+    });
+  } else {
+    obj.addEventListener(sEvent, fn, false);
   }
-  
-  myAddEvent = (obj, sEvent, fn) => {
-    if (obj.attachEvent) {
-      obj.attachEvent('on' + sEvent, function () {
-        fn.call(obj);
-      });
-    } else {
-      obj.addEventListener(sEvent, fn, false);
-    }
-  }
-  
-  startMove = (obj:any, json:any, fnEnd?:any) => {
-    if (obj.timer){
-      clearInterval(obj.timer);
-    } 
-  
-    obj.timer = setInterval(() => {
-      this.doMove(obj, json, fnEnd);
-    }, 30);
-  }
-  
-  getStyle = (obj, attr) => {
-    return obj.currentStyle
-      ? obj.currentStyle[attr]
-      : getComputedStyle(obj, false)[attr];
-  }
-  
-  /**
+}
+
+
+function getStyle(obj, attr){
+  return obj.currentStyle
+    ? obj.currentStyle[attr]
+    : getComputedStyle(obj, false)[attr];
+}
+
+function startMove(obj:any, json:any, fnEnd?:any){
+  if (obj.timer){
+    clearInterval(obj.timer);
+  } 
+
+  obj.timer = setInterval(() => {
+    doMove(obj, json, fnEnd);
+  }, 30);
+}
+
+/**
    * 整个动画播放的核心
    * @param obj
    * @param json 
    * @param fnEnd 
    */
-  doMove = (obj, json, fnEnd) => {
+  function doMove(obj, json, fnEnd){
     let iCur = 0;
     let attr = '';
     let bStop = true;
   
     for (attr in json) {
       attr === 'opacity'
-        ? (iCur = parseInt(100 * parseFloat(this.getStyle(obj, 'opacity'))))
-        : (iCur = parseInt(this.getStyle(obj, attr)));
+        ? (iCur = parseInt(100 * parseFloat(getStyle(obj, 'opacity'))))
+        : (iCur = parseInt(getStyle(obj, attr)));
   
       if (isNaN(iCur)) iCur = 0;
   
@@ -261,5 +302,3 @@ export class Carousel3D{
       fnEnd && fnEnd();
     }
   }
-
-}
